347f12281198eb8c6c756a4e8be4ea0df5ccef08
Nova Technology ltd <149790799+nova-technology-ltd@users.noreply.github.com>
2024-10-12 21:23:09 +0100

    reset password

--- features/auth/routes/userRoute.js
+++ features/auth/routes/userRoute.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.userLogin);
+router.post("/reset-password", userController.resetUserPassword);
 
 module.exports = router;
