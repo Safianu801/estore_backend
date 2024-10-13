9f28fc62a98435d1a356b338d71329d19397495b
Nova Technology ltd <149790799+nova-technology-ltd@users.noreply.github.com>
2024-10-13 01:12:13 +0100

    search feature

--- features/products/routes/productRoute.js
+++ features/products/routes/productRoute.js
@@ -3,5 +3,6 @@ const productController = require("../controller/productController");
 
 router.get("/p-product", productController.populateDB);
 router.get("/get-all-product", productController.getAllProduct);
+router.get("/search", productController.searchProducts);
 
 module.exports = router;
